f161070d36bb0deeed486110d59595008f41b822
Your Name <your.email@example.com>
2026-02-09 23:03:50 +0530

    push

--- tests/End-2-end/businessScenarioTest.spec.js
+++ tests/End-2-end/businessScenarioTest.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/LoginPage';
+import { LoginPage } from '../../pages/LoginPage.js';
 import { ProductPage } from '../../pages/productPage';
 import { CartPage } from '../../pages/CartPage';
 import { OrderProcess } from '../../pages/OrderProcess';
