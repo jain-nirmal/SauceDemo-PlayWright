f63e526d4e16ebe4288e541f7124b2c663c3c866
Your Name <your.email@example.com>
2026-02-09 22:21:09 +0530

    Delete old allure report

--- pages/OrderConfirmation.js
+++ pages/OrderConfirmation.js
@@ -2,7 +2,7 @@ class OrderConfirmationPage{
 
     constructor(page){
         this.page=page;
-        this.pageTitle=this.page.locator('.titles');
+        this.pageTitle=this.page.locator('.title');
         this.confirmationText=this.page.locator('.complete-header');
         this.confirmationMessage=this.page.locator('.complete-text');
         this.backHomeButton=this.page.locator('#back-to-products');
